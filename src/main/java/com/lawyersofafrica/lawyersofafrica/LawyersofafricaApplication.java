34bd789fa4e2fdd19cafb1687181dc6f9e41d224
unknown <lodkazibwe@gmail.com>
2022-04-15 23:05:33 +0300

    yes

--- src/main/java/com/lawyersofafrica/lawyersofafrica/LawyersofafricaApplication.java
+++ src/main/java/com/lawyersofafrica/lawyersofafrica/LawyersofafricaApplication.java
@@ -2,18 +2,24 @@ package com.lawyersofafrica.lawyersofafrica;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableScheduling
-public class LawyersofafricaApplication {
+public class LawyersofafricaApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(LawyersofafricaApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(LawyersofafricaApplication.class);
+    }
     @Bean
     public RestTemplate getRestTemplate(){return new RestTemplate();}
 
