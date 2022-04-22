e2637c5938c9e453d9bb232492117571b503a305
unknown <lodkazibwe@gmail.com>
2022-04-22 22:39:14 +0300

    admin

--- src/main/java/com/lawyersofafrica/lawyersofafrica/sysUser/security/MyUserDetails.java
+++ src/main/java/com/lawyersofafrica/lawyersofafrica/sysUser/security/MyUserDetails.java
@@ -32,7 +32,7 @@ public class MyUserDetails implements UserDetails {
 
     @Override
     public String getUsername() {
-        return user.getContact();
+        return user.getUserName();
     }
 
     @Override
